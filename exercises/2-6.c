a01b9c2f6022ca357ec21972982b9ee40ed657e3
Tony Martin <tony.martin@astehelsinki.fi>
2017-04-16 16:28:06 +0300

    2-7

--- exercises/2-6.c
+++ exercises/2-6.c
@@ -14,6 +14,13 @@ unsigned char setbits( unsigned char toBits, int pos, int numBits, unsigned char
   return ret;
 }
 
+unsigned char invert( unsigned char bits, int pos, int numBits ) {
+  
+  unsigned char mask = ~( ~0 << numBits ) << pos - numBits;
+  
+  return bits ^ mask;
+}
+
 void print_bits( unsigned char num ) {
 
   for ( int i = sizeof( num ) * 8; i >= 0; --i ) {
@@ -25,6 +32,8 @@ int main(int argc, char **argv) {
   
   unsigned char bits1 = 15;
   unsigned char bits2 = 22;
+  
+  // Exercise 2-6
   
   printf( "Before:\n" );
   printf( "Bits 1: " );
@@ -42,7 +51,18 @@ int main(int argc, char **argv) {
   printf( "\n" );
   printf( "Bits 2: " );
   print_bits( bits2 );
+  printf( "\n\n" );
+  
+  // Exercise 2-7
+  
+  printf( "Before: " );
+  unsigned char bits = 22;
+  print_bits( bits );
   printf( "\n" );
+  bits = invert( bits, 4, 2 );
+  printf( "After:  " );
+  print_bits( bits );
+  printf( "\n\n" );
   
   return 0;
 }
