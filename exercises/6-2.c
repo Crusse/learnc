163b0db2c4c7dc0e6e12a4b051afad299f416974
Tony Martin <crusse@elisanet.fi>
2017-12-09 23:28:23 +0200

    First working version of 6-2

--- exercises/6-2.c
+++ exercises/6-2.c
@@ -107,265 +107,6 @@ char *stack_current() {
 
 #endif
 
-char lastToken[ MAX_TOKEN_LEN + 1 ] = { 0 };
-
-int consume_space() {
-
-  char c;
-  int ret = 0;
-
-  while ( ( c = getc( stdin ) ) != EOF ) {
-    if ( isspace( c ) ) {
-      ret = 1;
-      continue;
-    }
-    ungetc( c, stdin );
-    break;
-  }
-
-  return ret;
-}
-
-int consume_char( char toConsume ) {
-
-  char c = getc( stdin );
-
-  if ( c == toConsume )
-    return 1;
-
-  ungetc( c, stdin );
-
-  return 0;
-}
-
-int is_identifier_start_char( char c ) {
-  return ( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c <= 'Z' ) || c == '_';
-}
-
-int parse_alphanum_token() {
-
-  char c;
-  int i = 0;
-
-  // A valid var name looks like ^[a-zA-Z_][a-zA-Z0-9_]*$
-  for ( ; i < MAX_TOKEN_LEN; ++i ) {
-
-    c = next_token_char();
-
-    if ( !c )
-      return 0;
-
-    lastToken[ i ] = c;
-
-    if ( !is_identifier_start_char( c ) || ( i != 0 && !( c >= '0' && c <= '9' ) ) ) {
-      ungetc( c, stdin );
-      return 0;
-    }
-  }
-
-  lastToken[ i ] = '\0';
-
-  if ( !lastToken[ 0 ] )
-    return 0;
-
-  return 1;
-}
-
-void parse_direct_declarator() {
-
-}
-
-void parse_pointer() {
-
-  
-}
-
-int parse_declarator() {
-
-  int ret = 0;
-
-  consume_space();
-
-  while ( parse_pointer() )
-    ;
-
-  consume_space();
-
-  
-}
-
-int parse_init_declarator() {
-
-  int ret = 0;
-
-  if ( parse_declarator() ) {
-
-    ret = 1;
-    consume_space();
-
-    // Discard initializer
-    if ( consume_char( '=' ) ) {
-
-      char c = '\0';
-      int initListNesting = 0;
-
-      while ( ( c = getc( stdin ) ) != EOF && c != ';' && ( initListNesting > 0 && c != ',' ) ) {
-        if ( c == '{' )
-          ++initListNesting;
-        else if ( c == '}' )
-          --initListNesting;
-      }
-
-      if ( c )
-        ungetc( c, stdin );
-    }
-  }
-
-  return ret;
-}
-
-int parse_init_declarator_list() {
-
-  int ret = 0;
-
-  while ( parse_init_declarator() ) {
-
-    ret = 1;
-    consume_space();
-
-    if ( !consume_char( ',' ) )
-      break;
-
-    consume_space();
-  }
-
-  return ret;
-}
-
-void parse_type_qualifier() {
-
-  static char strs[][] = {
-    "const",
-    "volatile",
-  };
-
-  consume_space();
-
-  if ( parse_alphanum_token() ) {
-    for ( int i = 0; i < sizeof strs / ( sizeof *strs ); ++i ) {
-      if ( strcmp( strs[ i ], lastToken ) == 0 )
-        return 1;
-    }
-  }
-
-  return 0;
-}
-
-void parse_type_specifier() {
-
-  // Doesn't support structs, unions, enums or typedefs
-  static char strs[][] = {
-    "void",
-    "char",
-    "short",
-    "int",
-    "long",
-    "float",
-    "double",
-    "signed",
-    "unsigned",
-  };
-
-  consume_space();
-
-  if ( parse_alphanum_token() ) {
-    for ( int i = 0; i < sizeof strs / ( sizeof *strs ); ++i ) {
-      if ( strcmp( strs[ i ], lastToken ) == 0 )
-        return 1;
-    }
-  }
-
-  return 0;
-}
-
-int parse_storage_class_specifier() {
-
-  // Doesn't support typedef
-  static char strs[][] = {
-    "auto",
-    "register",
-    "static",
-    "extern",
-  };
-
-  consume_space();
-
-  if ( parse_alphanum_token() ) {
-    for ( int i = 0; i < sizeof strs / ( sizeof *strs ); ++i ) {
-      if ( strcmp( strs[ i ], lastToken ) == 0 )
-        return 1;
-    }
-  }
-
-  return 0;
-}
-
-int parse_declaration_specifiers() {
-
-  int ret = 0;
-
-  while ( parse_storage_class_specifier() || parse_type_specifier() || parse_type_qualifier() )
-    ret = 1;
-
-  return ret;
-}
-
-int parse_declaration() {
-
-  if ( parse_declaration_specifiers() && parse_init_declarator_list() ) {
-
-    consume_space();
-
-    if ( consume_char( ';' ) ) {
-      
-    }
-  }
-}
-
-void parse_token() {
-
-  char c;
-  int i = 0;
-
-  // A valid var name looks like ^[a-zA-Z_][a-zA-Z0-9_]*$
-  for ( ; i < MAX_TOKEN_LEN; ++i ) {
-
-    c = getc( stdin );
-
-    if ( c == EOF ) {
-      token[ 0 ] = '\0';
-      return;
-    }
-
-    if ( !is_identifier_start_char( c ) || ( i != 0 && !( c >= '0' && c <= '9' ) ) )
-      break;
-
-    buffer[ i ] = c;
-  }
-
-  // Allow names for primitive vars and arrays, but not function declarations
-  if ( i == 0 || ( c != ',' && c != ')' && c != ';' ) ) {
-    buffer[ 0 ] = '\0';
-    return 0;
-  }
-
-  buffer[ i ] = '\0';
-
-  //printf( "Found name: %s\n", buffer );
-
-  return 1;
-}
-
 typedef struct tree_item {
   char *value;
   struct tree_item *left;
@@ -399,6 +140,7 @@ int tree_item_set_value( tree_item *item, const char *str ) {
 
 void tree_add_unique( tree_item *tree, const char *str ) {
 
+  // Already have this, not adding duplicates
   if ( tree->value && strcmp( tree->value, str ) == 0 )
     return;
 
@@ -417,77 +159,6 @@ void tree_add_unique( tree_item *tree, const char *str ) {
   }
 }
 
-/**
- * Returns the next non-whitespace, non-comment, non-string-literal character.
- */
-char next_token_char() {
-
-  char lastChar = '\0';
-  char c = '\0';
-
-  consume_space();
-
-  while ( 1 ) {
-
-    c = getc( stdin );
-
-    if ( c == EOF )
-      return '\0';
-
-    char nextC;
-
-    if ( c == '/' ) {
-      // Multi-line comment
-      if ( ( nextC = getc( stdin ) ) == '*' ) {
-        lastChar = '\0';
-        while ( 1 ) {
-          c = getc( stdin );
-          if ( c == EOF )
-            break;
-          if ( lastChar == '*' && c == '/' )
-            break;
-          lastChar = c;
-        }
-        continue;
-      }
-      else {
-        ungetc( nextC, stdin );
-      }
-    }
-    else if ( c == '/' ) {
-      // Single-line comment
-      if ( ( nextC = getc( stdin ) ) == '/' ) {
-        while ( ( c = getc( stdin ) ) != EOF && c != '\n' )
-          ;
-        continue;
-      }
-      else {
-        ungetc( nextC, stdin );
-      }
-    }
-    // String or character literal
-    else if ( c == '"' || c == '\'' ) {
-      char quoteChar = c;
-      int isEscaping = 0;
-      while ( ( c = getc( stdin ) ) != EOF && ( c != quoteChar || isEscaping ) ) {
-        if ( isEscaping )
-          isEscaping = 0;
-        else if ( c == '\\' )
-          isEscaping = 1;
-      }
-      continue;
-    }
-    else if ( isspace( c ) ) {
-      consume_space();
-      continue;
-    }
-
-    return c;
-  }
-
-  return '\0';
-}
-
 void _print_tree( tree_item *tree, int maxKeyLen, char *currKey, char *prevKey ) {
 
   if ( tree->left )
@@ -497,7 +168,7 @@ void _print_tree( tree_item *tree, int maxKeyLen, char *currKey, char *prevKey )
   strncpy( currKey, tree->value, maxKeyLen );
   currKey[ len ] = '\0';
 
-  if ( strcmp( key, prevKey ) != 0 )
+  if ( strcmp( currKey, prevKey ) != 0 )
     printf( "-----------------------\n" );
 
   printf( "%s\n", tree->value );
@@ -519,6 +190,352 @@ void print_tree( tree_item *tree, int maxKeyLen ) {
   _print_tree( tree, maxKeyLen, currKey, prevKey );
 }
 
+enum {
+  T_OTHER,
+  T_TYPE_SPECIFIER,
+  T_TYPE_QUALIFIER,
+  T_STORAGE_CLASS_SPECIFIER,
+  T_IDENTIFIER
+};
+
+tree_item *tree;
+char terminal[ MAX_TOKEN_LEN + 1 ];
+int terminalType = T_OTHER;
+
+int consume_space() {
+
+  char c;
+  int ret = 0;
+
+  while ( ( c = getc( stdin ) ) != EOF ) {
+    if ( isspace( c ) ) {
+      ret = 1;
+      continue;
+    }
+    ungetc( c, stdin );
+    break;
+  }
+
+  return ret;
+}
+
+/**
+ * Returns the next non-comment, non-string/char-literal character.
+ */
+int next_char() {
+
+  char c = '\0';
+
+  while ( 1 ) {
+
+    c = getc( stdin );
+
+    if ( c == EOF )
+      return c;
+
+    if ( c == '/' ) {
+
+      char lastChar;
+      char nextC = getc( stdin );
+      ungetc( nextC, stdin );
+
+      // Multi-line comment
+      if ( nextC == '*' ) {
+        lastChar = '\0';
+        while ( 1 ) {
+          c = getc( stdin );
+          if ( c == EOF )
+            break;
+          if ( lastChar == '*' && c == '/' )
+            break;
+          lastChar = c;
+        }
+        continue;
+      }
+      // Single-line comment
+      else if ( nextC == '/' ) {
+        while ( ( c = getc( stdin ) ) != EOF && c != '\n' )
+          ;
+        continue;
+      }
+
+    }
+    // String or character literal
+    else if ( c == '"' || c == '\'' ) {
+
+      char quoteChar = c;
+      int isEscaping = 0;
+      while ( ( c = getc( stdin ) ) != EOF && ( c != quoteChar || isEscaping ) ) {
+        if ( isEscaping )
+          isEscaping = 0;
+        else if ( c == '\\' )
+          isEscaping = 1;
+      }
+      continue;
+    }
+
+    return c;
+  }
+
+  return EOF;
+}
+
+int consume_char( char toConsume ) {
+
+  char c = next_char();
+
+  if ( c == toConsume )
+    return 1;
+
+  ungetc( c, stdin );
+
+  return 0;
+}
+
+int is_identifier_start_char( char c ) {
+  return ( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c <= 'Z' ) || c == '_';
+}
+
+int is_type_qualifier( char *token ) {
+
+  static const char *strs[] = {
+    "const",
+    "volatile",
+  };
+
+  for ( int i = 0; i < sizeof strs / ( sizeof *strs ); ++i ) {
+    if ( strcmp( strs[ i ], token ) == 0 )
+      return 1;
+  }
+
+  return 0;
+}
+
+int is_type_specifier( char *token ) {
+
+  // Doesn't support structs, unions, enums or typedefs
+  static const char *strs[] = {
+    "void",
+    "char",
+    "short",
+    "int",
+    "long",
+    "float",
+    "double",
+    "signed",
+    "unsigned",
+  };
+
+  for ( int i = 0; i < sizeof strs / ( sizeof *strs ); ++i ) {
+    if ( strcmp( strs[ i ], token ) == 0 )
+      return 1;
+  }
+
+  return 0;
+}
+
+int is_storage_class_specifier( char *token ) {
+
+  // Doesn't support typedef
+  static const char *strs[] = {
+    "auto",
+    "register",
+    "static",
+    "extern",
+  };
+
+  for ( int i = 0; i < sizeof strs / ( sizeof *strs ); ++i ) {
+    if ( strcmp( strs[ i ], token ) == 0 )
+      return 1;
+  }
+
+  return 0;
+}
+
+int next_alphanum_terminal() {
+
+  char c;
+  int i = 0;
+
+  for ( ; i < MAX_TOKEN_LEN; ++i ) {
+
+    c = next_char();
+
+    if ( c == EOF )
+      return 0;
+
+    terminal[ i ] = c;
+
+    // A valid var name looks like ^[a-zA-Z_][a-zA-Z0-9_]*$
+    if ( !is_identifier_start_char( c ) && ( i == 0 || !( c >= '0' && c <= '9' ) ) ) {
+      ungetc( c, stdin );
+      break;
+    }
+  }
+
+  terminal[ i ] = '\0';
+
+  if ( is_type_qualifier( terminal ) )
+    terminalType = T_TYPE_QUALIFIER;
+  else if ( is_type_specifier( terminal ) )
+    terminalType = T_TYPE_SPECIFIER;
+  else if ( is_storage_class_specifier( terminal ) )
+    terminalType = T_STORAGE_CLASS_SPECIFIER;
+  else
+    terminalType = T_IDENTIFIER;
+
+  // Empty string
+  if ( !terminal[ 0 ] )
+    return 0;
+
+  return 1;
+}
+
+int parse_pointer() {
+
+  int ret = 0;
+
+  consume_space();
+
+  while ( consume_char( '*' ) ) {
+    ret = 1;
+    consume_space();
+    while ( next_alphanum_terminal() && terminalType == T_TYPE_QUALIFIER && consume_space() )
+      ;
+  }
+
+  return ret;
+}
+
+int parse_direct_declarator() {
+
+  consume_space();
+
+  if ( next_alphanum_terminal() ) {
+
+    if ( terminalType != T_IDENTIFIER )
+      return 0;
+    
+    consume_space();
+
+    // Ignore function declarations
+    if ( consume_char( '(' ) )
+      return 0;
+
+    tree_add_unique( tree, terminal );
+
+    return 1;
+  }
+  // Parenthesized variable name
+  else if ( consume_char( '(' ) ) {
+    
+    int parse_declarator();
+
+    return parse_declarator();
+  }
+
+  return 0;
+}
+
+int parse_declarator() {
+
+  int ret = 0;
+
+  while ( parse_pointer() )
+    ;
+
+  if ( parse_direct_declarator() )
+    ret = 1;
+  else
+    ret = 0;
+
+  return ret;
+}
+
+int parse_init_declarator() {
+
+  int ret = 0;
+
+  if ( parse_declarator() ) {
+
+    ret = 1;
+    consume_space();
+
+    // Discard initializer
+    if ( consume_char( '=' ) ) {
+
+      char c = '\0';
+      int initListNesting = 0;
+
+      while ( ( c = next_char() ) != EOF && c != ';' && ( c != ',' || initListNesting > 0 ) ) {
+        if ( c == '{' )
+          ++initListNesting;
+        else if ( c == '}' )
+          --initListNesting;
+      }
+
+      if ( c )
+        ungetc( c, stdin );
+    }
+  }
+
+  return ret;
+}
+
+int parse_init_declarator_list() {
+
+  int ret = 0;
+
+  while ( parse_init_declarator() ) {
+
+    ret = 1;
+    consume_space();
+
+    if ( !consume_char( ',' ) )
+      break;
+
+    consume_space();
+  }
+
+  return ret;
+}
+
+int parse_declaration_specifiers() {
+
+  int ret = 0;
+
+  while ( next_alphanum_terminal() &&
+    ( terminalType == T_STORAGE_CLASS_SPECIFIER ||
+      terminalType == T_TYPE_SPECIFIER ||
+      terminalType == T_TYPE_QUALIFIER ) )
+  {
+    ret = 1;
+  }
+
+  return ret;
+}
+
+int parse_declaration() {
+
+  if ( parse_declaration_specifiers() && consume_space() && parse_init_declarator_list() ) {
+
+    consume_space();
+
+    if ( consume_char( ';' ) )
+      return 1;
+  }
+
+  return 0;
+}
+
+void populate_tree() {
+
+  while ( !feof( stdin ) ) {
+    if ( !parse_declaration() )
+      next_char();
+  }
+}
+
 int main( int argc, char *argv[] ) {
 
   int keyLength = DEFAULT_GROUP_KEY_LENGTH; 
@@ -535,9 +552,8 @@ int main( int argc, char *argv[] ) {
     }
   }
 
-  tree_item *tree = tree_item_create();
-
-  populate_tree( tree );
+  tree = tree_item_create();
+  populate_tree();
   print_tree( tree, keyLength );
 
   return 0;
